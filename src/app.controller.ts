b7e9b57f9d376aa90de2d0c827296126263bbea7
Sarfraz <m.sarfraz.arshad@gmail.ocm>
2023-06-05 12:23:25 +0500

    Create Login dto and associate it in the login endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Request, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiProperty } from '@nestjs/swagger';
+
+class LoginDto{
+  @ApiProperty()
+  username: string;
+
+  @ApiProperty()
+  password: string;
+}
 
 @Controller()
 export class AppController {
@@ -8,5 +18,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req,  @Body() loginDto: LoginDto) {
+    return req.user;
   }
 }
